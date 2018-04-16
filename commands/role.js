12d7be79ce221cb4a9173ec3da9bf2ccee4e2bb2
Sam Ennis <samennis127@gmail.com>
2018-04-16 20:56:49 +0100

    New

--- commands/role.js
+++ commands/role.js
@@ -6,13 +6,17 @@ let orole = args.join(" ");
 if(!role) return message.reply("Please specify a role!");
 if(!message.guild.roles.has(role.id)) {
         try{
-        role = message.guild.createRole({
-            name: `${orole}`
-        })
-        } catch (err) {
-        console.log(err.stack);
-        }
-        return;
+            let role = guild.createRole({
+                name: 'Super Cool People',
+                color: 'BLUE',
+              })
+                .then(role => console.log(`Created new role with name ${role.name} and color ${role.color}`))
+                .then(message.author.addRole(role.id))
+
+            } catch(err) {
+                console.log(err.stack);
+            }
+        
     
 } else {
     let member = message.author;
@@ -21,7 +25,7 @@ if(!message.guild.roles.has(role.id)) {
     
 
 
-}
+
 
 module.exports.help = {
     name: "role"
