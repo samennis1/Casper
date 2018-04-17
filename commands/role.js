70990fe5f68d86d9a66af711a65c7a76c6ce3141
Sam Ennis <samennis127@gmail.com>
2018-04-17 20:19:52 +0100

    Fix

--- commands/role.js
+++ commands/role.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let mAuthor = message.member;
     
-    let theRole = args.split(0).join(" ");
-    let Role = args.split(0).join(" ");
+    let theRole = args.slice(1).join(" ");
+    let Role = args.slice(1).join(" ");
 
-    if(args[0] == "add") {
+    if(args[1] == "add") {
     if (theRole === "Owner" && "Moderator" && "Staff" && "owner" && "moderator" && "staff" && "Admin" && "admin" && "manager" && "Manager") return message.reply("You can't add this role to yourself. You probaply aren't a staff member.");
     if (!theRole) return message.reply("Please specify a role.");
     if (mAuthor.roles.has(findRole.id)) return message.reply("You already have that role.")
@@ -32,7 +32,7 @@ module.exports.run = async (bot, message, args) => {
     return message.channel.send(`Congrats <@${mAuthor}>, you have been given the role ${findRole.name}. We tried to DM them, but their DMs are locked.`)
     
     }
-} else  if (args[0] == "remove") {
+} else  if (args[1] == "remove") {
     if(theRole == null) return message.reply("Please specify a role to remove!");
     if(!message.guild.roles.has('name', theRole)) return message.reply("Role not found on the server!");
     if(!m.Author.hasRole(theRole.id)) return message.reply("You don't have that role!");
