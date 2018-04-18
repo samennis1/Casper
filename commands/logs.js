91f41419d90434e9d9017798c63d1277d5296d30
Sam Ennis <samennis127@gmail.com>
2018-04-18 18:55:35 +0100

    .

--- commands/logs.js
+++ commands/logs.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
     if(!author.hasPermission("ADMINISTRATOR")) return message.reply("No Permission!");
     if(args > 0 || args < 0) return message.reply("Invalid Arguments!");
     let channel = args[0];
-    let findC = message.guild.channels.find("name", `channel`);
+    let findC = message.guild.channels.find("name", `${channel}`);
 
     if(!findC) return message.reply("No channel found!");
    botconfig.logchannel = channel;
