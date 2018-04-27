81d7f0a8b901e8dd078abc0688b044fd6295b6c8
Sam Ennis <samennis127@gmail.com>
2018-04-27 20:50:50 +0100

    Fix

--- commands/logs.js
+++ commands/logs.js
@@ -7,16 +7,12 @@ module.exports.run = async (bot, message, args) => {
     if(!author.hasPermission("ADMINISTRATOR")) return message.reply("No Permission!");
     if(args > 0 || args < 0) return message.reply("Invalid Arguments!");
     let channel = args[0];
-    let findC = message.guild.channels.get("id", channel);
+    let findC = message.guild.channels.get("name", channel);
 
     if(!findC) return message.reply("No channel found!");
    botconfig.logchannel = channel;
 
-   fs.writeFile("../botconfig.json", JSON.stringify(botconfig.logchannel), function(error){
-    if(error) throw error;
-
-});
-
+   fs.writeFile("../botconfig.json", JSON.stringify(botconfig));
 
 }
 
