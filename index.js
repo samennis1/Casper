76135124c6d014248d31312b4d0fd9d3e0c29ede
Sam Ennis <samennis127@gmail.com>
2018-04-29 19:48:21 +0100

    Fix

--- index.js
+++ index.js
@@ -30,14 +30,12 @@ bot.on("ready", async() => {
 console.log(`${bot.user.username} is online`)
 bot.user.setActivity(`${botconfig.prefix}help`)
 let a = bot.channels.find(`name`, "changes");
-let b = bot.channels.find(`name`, "#🆕▸server-updates");
 let online = new Discord.RichEmbed()
 .setDescription("Github Update Pushed")
 .setColor("#ff0000")
 .addField("Bot online", "New update was pushed to Github");
 
 a.send(online).then(msg => msg.delete(5000));
-b.send(online).then(msg => msg.delete(5000));
 
 bot.on("message", async message => {
   if(message.author.bot) return;
