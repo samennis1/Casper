fe1d14263a0e625d6dc91b0c87b7b141685024d5
Sam Ennis <samennis127@gmail.com>
2018-04-29 15:57:41 +0100

    Fix

--- commands/colours.js
+++ commands/colours.js
@@ -7,14 +7,14 @@ module.exports.run = async (bot, message, args) => {
     .setColor("#ff0000")
     .setThumbnail(bot.user.avatarURL)
     .setDescription(`You can choose one of the following colours`)
-    .addField("Red", "")
-    .addField("Orange", "")
-    .addField("Yellow", "")
-    .addField("Green", "")
-    .addField("Light Blue", "")
-    .addField("Dark Blue", "")
-    .addField("Pink", "")
-    .addField("Purple", "");
+    .addField("Red", " ")
+    .addField("Orange", " ")
+    .addField("Yellow", " ")
+    .addField("Green", " ")
+    .addField("Light Blue", " ")
+    .addField("Dark Blue", " ")
+    .addField("Pink", " ")
+    .addField("Purple", " ");
     message.channel.send("A list of colours has been DM'd to you!");
     return message.member.send(a);
 }
