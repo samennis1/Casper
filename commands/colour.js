f593f75f79f4a3de302c0bc7ccd8fce68b0b23b3
Sam Ennis <samennis127@gmail.com>
2018-04-29 19:50:50 +0100

    Major change to colour

--- commands/colour.js
+++ commands/colour.js
@@ -39,54 +39,27 @@ if(!colourrole) {
         return message.channel.send(a);
     }
 } else {
-    if(args[0] == "set") {
-
-
-        if(user.roles.has(colourrole)) {
-            let a = new Discord.RichEmbed()
-            .setTitle("Casper | ERROR")
-            .setColor("#ff0000")
-            .setThumbnail(bot.user.avatarURL)
-            .setDescription(`You already have this colour! To remove it do ${botconfig.prefix}remove!`);
-            return message.channel.send(a).then(msg => msg.delete(5000));
-        } else if(user.roles.find("name", "Red") && user.roles.find("name", "Orange") && user.roles.find("name", "Yellow") && user.roles.find("name", "Green") && user.roles.find("name", "Light Blue") && user.roles.find("name", "Dark Blue") && user.roles.find("name", "Pink") && user.roles.find("name", "Purple")) {
-            let a = new Discord.RichEmbed()
-            .setTitle("Casper | ERROR")
-            .setColor("#ff0000")
-            .setThumbnail(bot.user.avatarURL)
-            .setDescription(`Please remove your current colour by using ${botconfig.prefix}remove!`);
-            return message.channel.send(a).then(msg => msg.delete(5000));
-        } else {
-            user.addRole(colourrole);
-            let a = new Discord.RichEmbed()
-            .setTitle("Casper | Success")
-            .setColor("#00ff00")
-            .setThumbnail(bot.user.avatarURL)
-            .setDescription(`Colour has been added!`);
-            return message.channel.send(a).then(msg => msg.delete(5000));
-        }
-    } else if (args[0] == "remove") {
-          if(user.roles.has(colourrole)) {
-            let a = new Discord.RichEmbed()
-            .setTitle("Casper | ERROR")
-            .setColor("#ff0000")
-            .setThumbnail(bot.user.avatarURL)
-            .setDescription(`You don't have this colour role!`);
-            return message.channel.send(a).then(msg => msg.delete(5000));
-          } else {
-              user.removeRole(colourrole);
-          }
-        } else {
-            let a = new Discord.RichEmbed()
-            .setTitle("Casper | ERROR")
-            .setColor("#ff0000")
-            .setThumbnail(bot.user.avatarURL)
-            .setDescription(`Please provide proper arguments! ${botconfig.prefix}colour <Set/Remove> <Colour>`);
-            return message.channel.send(a).then(msg => msg.delete(5000));
-        }
+    if(user.roles.has(colourrole)) {
+        user.removeRole(colourrole)
+        let a = new Discord.RichEmbed()
+        .setTitle("Casper | Colour Removed")
+        .setColor("#ff0000")
+        .setThumbnail(bot.user.avatarURL)
+        .setDescription(`Colour removed`);
+        return message.channel.send(a);
+    } else {
+        user.addRole(colourrole)
+        let a = new Discord.RichEmbed()
+        .setTitle("Casper | ERROR")
+        .setColor("#ff0000")
+        .setThumbnail(bot.user.avatarURL)
+        .setDescription(`Colour <@${colourrole.id}> has been added!`);
+        return message.channel.send(a);
     }
-}
 
+    
+}
+}
 
 
 module.exports.help = {
