563e916ad30981b464d6be84c784171675c1f78c
Sam Ennis <samennis127@gmail.com>
2018-04-30 20:09:18 +0100

    Fix

--- index.js
+++ index.js
@@ -99,9 +99,11 @@ bot.on("message", async message => {
       sql = `UPDATE xp SET xp = ${xp + genXp()} WHERE id = ${message.author.id}`
 
       if(xp > 1000) {
-        if(message.member.roles.find(a)) return;
-       let a = message.guild.roles.find("name", "Level 1") 
+        let a = message.guild.roles.find("name", "Level 1") 
+    
+
        if(!a) return;
+       if(message.member.roles.find(a)) return;
 
        message.member.addRole(a);
        let levelup = new Discord.RichEmbed()
@@ -118,9 +120,11 @@ bot.on("message", async message => {
       }
 
       if(xp >= 2000) {
-        if(message.member.roles.find(b)) return;
         let a = message.guild.roles.find("name", "Level 2") 
+        if(message.member.roles.find(a)) return;
+
         if(!a) return;
+        if(message.member.roles.find(a)) return;
     
         message.member.removeRole(a);
         message.member.addRole(b);
