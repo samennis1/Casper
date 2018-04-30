95792d068a46221d857d3f227376300d69403601
Sam Ennis <samennis127@gmail.com>
2018-04-30 19:18:42 +0100

    Fix

--- commands/xp.js
+++ commands/xp.js
@@ -8,14 +8,20 @@ module.exports.run = async (bot, message, args, con) => {
     message.delete();
 
     con.query(`SELECT * FROM xp WHERE id = ${target.id}`, (err, rows ) => {
-
-        let xp = rows[0].xp;
-
-        if(xp == null) return;
-
         if(err) throw err;
 
+ if(!rows[0]) {
+    let a = new Discord.RichEmbed()
+    .setTitle("XP Level")
+    .setColor("#00ff00")
+    .setFooter("Current XP Level")
+    .setDescription("XP Level => None Yet :( ");
 
+    message.channel.send(a).then(msg => msg.delete(5000));
+    return;
+ }
+
+      let xp = rows[0].xp;
 
 
       
