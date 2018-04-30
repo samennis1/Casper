50dd9ae50389ba4127c6d270af394e3d7a1e500f
Sam Ennis <samennis127@gmail.com>
2018-04-30 17:23:25 +0100

    Fix

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ a.send(online).then(msg => msg.delete(5000));
 });
 
 var con = mysql.createConnection({
-  host: "99.000webhost.io:3306",
+  host: "99.000webhost.io",
+  port: "3306",
   user: "id5584969_samennis1",
   password: `${password}`,
   database: "sadb"
