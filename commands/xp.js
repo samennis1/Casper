c137f6a0b2948e40a7605595cf74aef625bb9610
Sam Ennis <samennis127@gmail.com>
2018-04-30 18:58:39 +0100

    Fix

--- commands/xp.js
+++ commands/xp.js
@@ -1,4 +1,4 @@
-module.exports.run = async (bot, message, args, con) {
+module.exports.run = async (bot, message, args, con) => {
     let target = message.mentions.users.first() || message.guild.members.get(args[1]) || message.author;
 
     con.query(`SELECT * FROM xp WHERE id = ${target.id}`, (err, rows ) => {
